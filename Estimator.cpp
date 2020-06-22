18e44cf00537d52ed0f92dc4f27f16bb1b78eb96
DeadPi <53448898+DeadPi@users.noreply.github.com>
2020-06-22 22:44:27 +0200

    Update Estimator.cpp

--- Estimator.cpp
+++ Estimator.cpp
@@ -4,7 +4,7 @@
 #include <time.h>
 #include <ctime>
 
-using namespace std;
+using namespace std; // yeah i know i shouldn't use this
 
 float estimate_pi(int n) {
 	float inner_points = 0;
@@ -15,13 +15,14 @@ float estimate_pi(int n) {
 		
 		float x = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
 		float y = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-
+		//gets 2 random floats from 0 to 1
 		float distance = pow(x, 2) + pow(y, 2);
-
+		//calculates the total distance to the origin of the grid
 		if (distance < 1) {
 			inner_points = inner_points + 1;
 		}
 		points_total = points_total + 1;
+		//checks if the coordinate is still in the radius or not
 	}
 	float pi = 4*(inner_points / points_total);
 	return pi;
